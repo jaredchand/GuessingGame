a967864469ce7f4a349c8d4728e47c961ae56d76
Jared Chand <jc494084@ad.jcu.edu.au>
2020-03-03 10:52:54 +1000

    Initial commit

---
+++ app/src/main/java/au/edu/jcu/cp3406/guessinggame/MainActivity.java
@@ -0,0 +1,13 @@
+package au.edu.jcu.cp3406.guessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
